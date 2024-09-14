b4cc0b8a2c6b40d8f1db50ccc40a8c88a3aadadc
chandan singh <sc231997@gmail.com>
2024-09-14 13:40:38 +0530

    simple movement code added

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
@@ -16,10 +17,13 @@ import com.csjbot.coshandler.core.Action;
 import com.csjbot.coshandler.core.CsjRobot;
 import com.csjbot.coshandler.listener.OnAuthenticationListener;
 
+import java.util.Objects;
+
 public class MainActivity extends AppCompatActivity {
 
-    private static final String API_KEY = "";
-    private static final String USER_KEY = "";
+    private static final String API_KEY = "0d3579e3-6185-4f51-851e-a8c9bc13f2b2";
+    private static final String USER_KEY = "80919167EBF6D9A2B470B3262E2A841D";
+    TextView tvApiKey, tvUserKey;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,7 +36,36 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
 
-        CsjRobot.authentication(this, API_KEY, USER_KEY, new OnAuthenticationListener() {
+        tvApiKey = findViewById(R.id.editTextApiKey);
+        tvUserKey = findViewById(R.id.editTextUserKey);
+
+        tvApiKey.setText(API_KEY);
+        tvUserKey.setText(USER_KEY);
+    }
+
+    public void handleMovement(View view) {
+        Button button = (Button) view;
+        Action action = CsjRobot.getInstance().getAction();
+        int id = button.getId();
+        if (id == R.id.forward) {
+            Toast.makeText(this, "Button clicked! " + button.getText(), Toast.LENGTH_SHORT).show();
+            action.moveForward();
+        } else if (id == R.id.backward) {
+            Toast.makeText(this, "Button clicked! " + button.getText(), Toast.LENGTH_SHORT).show();
+            action.moveBack();
+        } else if (id == R.id.left) {
+            Toast.makeText(this, "Button clicked! " + button.getText(), Toast.LENGTH_SHORT).show();
+            action.moveLeft();
+        } else if (id == R.id.right) {
+            Toast.makeText(this, "Button clicked! " + button.getText(), Toast.LENGTH_SHORT).show();
+            action.moveRight();
+        }
+    }
+
+    public void handleSave(View view) {
+        Button button = (Button) view;
+
+        CsjRobot.authentication(this, tvApiKey.getText().toString(), tvUserKey.getText().toString(), new OnAuthenticationListener() {
             @Override
             public void success() {
                 Log.d("TAG", "Authorization succeeded!");
@@ -50,24 +83,12 @@ public class MainActivity extends AppCompatActivity {
             e.printStackTrace();
         }
 
-//        CsjRobot.enableSlam(true);
-        CsjRobot.setRobotType(CsjRobot.RobotType.TIMO);
-//        CsjRobot.getInstance().init(this);
-//        Action action = CsjRobot.getInstance().getAction();
-//        action.moveForward();
-    }
-
-    public void handleButtonClick(View view) {
-        Button button = (Button) view;
-        int id = button.getId();
-        if (id == R.id.forward) {
-            Toast.makeText(this, "Button clicked! " + button.getText(), Toast.LENGTH_SHORT).show();
-        } else if (id == R.id.backward) {
-            Toast.makeText(this, "Button clicked! " + button.getText(), Toast.LENGTH_SHORT).show();
-        } else if (id == R.id.left) {
-            Toast.makeText(this, "Button clicked! " + button.getText(), Toast.LENGTH_SHORT).show();
-        } else if (id == R.id.right) {
-            Toast.makeText(this, "Button clicked! " + button.getText(), Toast.LENGTH_SHORT).show();
+        CsjRobot.setRobotType(CsjRobot.RobotType.ALICE);
+        try {
+            CsjRobot.getInstance().init(this);
+        } catch (Exception e) {
+            Log.e("Tag", Objects.requireNonNull(e.getMessage()));
+            Toast.makeText(this, Objects.requireNonNull(e.getMessage()), Toast.LENGTH_LONG).show();
         }
     }
 }
